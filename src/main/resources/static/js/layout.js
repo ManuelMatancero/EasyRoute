986a42e1cb38d837bc398f2fd0f87ef403ff43c1
Frainer Encarnación <fraineralex2001@gmail.com>
2023-08-06 10:35:19 -0400

    add: aply the dark mode to the table when is actived

--- src/main/resources/static/js/layout.js
+++ src/main/resources/static/js/layout.js
@@ -1,32 +1,41 @@
 const body = document.querySelector("body"),
-      modeToggle = body.querySelector(".mode-toggle");
-      sidebar = body.querySelector("nav");
-      sidebarToggle = body.querySelector(".sidebar-toggle");
+  modeToggle = body.querySelector(".mode-toggle");
+sidebar = body.querySelector("nav");
+sidebarToggle = body.querySelector(".sidebar-toggle");
+tableElement = body.querySelector("table");
+tableContainer = body.querySelector(".table-container");
+tableContainer2 = body.querySelector(".table-container2");
 
 let getMode = localStorage.getItem("mode");
-if(getMode && getMode ==="dark"){
-    body.classList.toggle("dark");
+if (getMode && getMode === "dark") {
+  body.classList.toggle("dark");
+  tableElement.classList.toggle("table-dark");
+  tableContainer.classList.toggle("bg-dark");
+  tableContainer2.classList.toggle("bg-dark");
 }
 
 let getStatus = localStorage.getItem("status");
-if(getStatus && getStatus ==="close"){
-    sidebar.classList.toggle("close");
+if (getStatus && getStatus === "close") {
+  sidebar.classList.toggle("close");
 }
 
-modeToggle.addEventListener("click", () =>{
-    body.classList.toggle("dark");
-    if(body.classList.contains("dark")){
-        localStorage.setItem("mode", "dark");
-    }else{
-        localStorage.setItem("mode", "light");
-    }
+modeToggle.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  tableElement.classList.toggle("table-dark");
+  tableContainer.classList.toggle("bg-dark");
+  tableContainer2.classList.toggle("bg-dark");
+  if (body.classList.contains("dark")) {
+    localStorage.setItem("mode", "dark");
+  } else {
+    localStorage.setItem("mode", "light");
+  }
 });
 
 sidebarToggle.addEventListener("click", () => {
-    sidebar.classList.toggle("close");
-    if(sidebar.classList.contains("close")){
-        localStorage.setItem("status", "close");
-    }else{
-        localStorage.setItem("status", "open");
-    }
-})
+  sidebar.classList.toggle("close");
+  if (sidebar.classList.contains("close")) {
+    localStorage.setItem("status", "close");
+  } else {
+    localStorage.setItem("status", "open");
+  }
+});
